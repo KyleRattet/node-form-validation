b60353fdbc338a553f5c120a2a73886afe1d6443
KyleRattet <krattet@gmail.com>
2015-08-22 12:44:38 -0600

    add person option built out

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 var puppyArray = [];
+var peopleArray = [];
 // var idArray = [];
 
-//on submit, route to puppynew form
+//on submit, route to puppynew or personnew form
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Add A Puppy' });
 });
+
+
+/////////////
+/// puppy ///
+/////////////
 
 //create new puppy form, take input and render to puppy table
 router.post('/puppy/new', function(req, res, next) {
@@ -17,7 +23,7 @@ router.post('/puppy/new', function(req, res, next) {
   // idArray.push(req.body.id);
 });
 
-//puppy table of all puppies
+//puppy list of all puppies
 router.post('/puppies', function(req, res, next) {
   //push form data into arrays to display
   puppyArray.push(req.body);
@@ -35,11 +41,34 @@ router.post('/puppies', function(req, res, next) {
 });
 
 
+//////////////
+/// person ///
+//////////////
 
+//create new person form, take input and render to people table
+router.post('/people/new', function(req, res, next) {
+  res.render('personnew', { title: 'Add A Person' });
+  // console.log(req.body);
+  // puppyArray.push(req.body.name);
+  // idArray.push(req.body.id);
+});
 
+//people list of all people
+router.post('/people', function(req, res, next) {
+  //push form data into arrays to display
+  peopleArray.push(req.body);
+  // idArray.push(req.body.id);
+  console.log(peopleArray);
+  // console.log(idArray);
 
+  //render people html
+  res.render('people', {
+    title: 'Showing People',
+    peopleArray: peopleArray,
+    // idArray: idArray
+  });
 
-
+});
 
 
 module.exports = router;
