fb915c38d9f966eb2ce929f5a52144bb88525de4
KyleRattet <krattet@gmail.com>
2015-08-22 11:17:48 -0600

    getting puppy name and id array to render on /puppies

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+var puppyArray = [];
+var idArray = [];
+
+//on submit, route to puppynew form
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Add A Puppy' });
 });
 
+//create new puppy form, take input and render to puppy table
 router.post('/puppy/new', function(req, res, next) {
   res.render('puppynew', { title: 'Add A Puppy' });
+  // console.log(req.body);
+  // puppyArray.push(req.body.name);
+  // idArray.push(req.body.id);
 });
+
+//puppy table of all puppies
+router.post('/puppies', function(req, res, next) {
+  console.log(req.body);
+  puppyArray.push(req.body.name);
+  idArray.push(req.body.id);
+  console.log(puppyArray);
+  console.log(idArray);
+
+  res.render('puppies', {
+    title: 'Puppy Table',
+    name: puppyArray,
+    id: idArray });
+
+});
+
+
+
+
 
 
 
