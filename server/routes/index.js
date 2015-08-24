49d277e7fb9e149ecfa61519caaebead9cb302cd
KyleRattet <krattet@gmail.com>
2015-08-24 15:47:31 -0600

    updated validation for puppy

--- server/routes/index.js
+++ server/routes/index.js
@@ -18,6 +18,7 @@ router.get('/', function(req, res, next) {
 //create new puppy form, take input and render to puppy table
 router.post('/puppy/new', function(req, res, next) {
   res.render('puppynew', { title: 'Add A Puppy' });
+
   // console.log(req.body);
   // puppyArray.push(req.body.name);
   // idArray.push(req.body.id);
@@ -25,7 +26,21 @@ router.post('/puppy/new', function(req, res, next) {
 
 //puppy list of all puppies
 router.post('/puppies', function(req, res, next) {
+
+  console.log(req.body.name);
+  console.log(req.body.id);
+  var errors = puppyValidationCheck(req.body.name, req.body.id);
+  console.log(errors);
   //push form data into arrays to display
+
+  if (errors.length > 0) {
+
+    res.render('puppynew', {
+      title: 'Add A Puppy',
+      errors : errors
+    });
+
+  } else {
   puppyArray.push(req.body);
   // idArray.push(req.body.id);
   console.log(puppyArray);
@@ -34,9 +49,12 @@ router.post('/puppies', function(req, res, next) {
   //render puppies html
   res.render('puppies', {
     title: 'Showing Puppies',
+    success: "The puppy was saved successfully.",
     puppyArray: puppyArray,
     // idArray: idArray
   });
+
+ }
 
 });
 
@@ -67,10 +85,35 @@ router.post('/people', function(req, res, next) {
   res.render('people', {
     title: 'Showing People',
     peopleArray: peopleArray,
+    success: "The person was saved successfully.",
     // idArray: idArray
   });
 
 });
+
+//add puppy validation check function, check to see if array is full,
+
+function puppyValidationCheck(puppyName, puppyID) {
+
+  var errorArray = [];
+  var puppyNameTrimmed = puppyName.trim();
+  var puppyIDTrimmed = puppyID.trim();
+
+//puppy name validations
+  if (puppyNameTrimmed === '') {
+    errorArray.push("Name cannot be blank.");
+  }
+
+//puppy ID validations
+  if (puppyIDTrimmed === '') {
+    errorArray.push('Tag cannot be blank.');
+  } else if (puppyIDTrimmed.length < 3) {
+    errorArray.push('A Tag must be at least 3 characters long.');
+  }
+
+  return errorArray;
+
+}
 
 
 module.exports = router;
